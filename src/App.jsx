b343271a4de5f96e7ea90b0ec1104824a86933b4
ElenaSV828 <suslovae180@gmail.com>
2024-11-12 18:40:41 +0100

    Add

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Button from "./components/Button/Button";
+import LessondCard from "./components/Button/LessondCard/lessondCard";
+function App() {
+  return  <div className="App">
+<LessondCard/>
+<Button/>
+<Button/>
+  </div>
+    
+  
+}
+
+export default App;
---
+++ src/components/Button/LessondCard/lessondCard.jsx
@@ -0,0 +1,13 @@
+import Button from "../Button/Button";
+import "styles.css";
+
+function LessondCard(){
+return(
+ <div classname ="lessond-card">   
+<h3>Lesson1</h3>
+<Button/>
+</div>
+);
+}
+
+export default LessondCard;
