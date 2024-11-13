9c1accba322b855a26f32e7b530bd9ae7d2bb0f2
ElenaSV828 <suslovae180@gmail.com>
2024-11-13 17:53:04 +0100

    Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import "./App.css";
-import Button from "./components/Button/Button";
-import LessondCard from "./components/Button/LessondCard/lessondCard";
+
+//lesson imports
+import Lesson02 from "./lessons/Lesson02/Lesson02";
+import Lesson03 from "./lessons/Lesson03/Lesson03";
+
+//homework imports
+import Homework02 from "./homeworks/Homework02/Homework02";
+
+//consultation imports
+
+
 function App() {
-  return  <div className="App">
-<LessondCard/>
-<Button/>
-<Button/>
-  </div>
-    
-  
+  return (
+    <div className="App">
+      {/* <Lesson02 /> */}
+      {/* <Homework02 /> */}
+      <Lesson03/>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/lessons/Lesson02/Lesson02.jsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+import Button from "../../components/Button/Button";
+import LessondCard from "../../components/LessondCard/lessondCard";
+
+function Lesson02() {
+ return (
+    <div className="lesson02-wrapper">
+      <LessondCard />
+      <Button />
+      <Button />
+    </div>
+  );
+}
+export default Lesson02;
